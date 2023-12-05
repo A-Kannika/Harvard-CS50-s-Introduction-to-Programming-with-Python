93ca10b1c9ca88e8382fc2e78b893009c0e581c6
akannika <a.kannika80@gmail.com>
2023-12-04 17:56:36 -0800

    Change the filename and create function

---
+++ 3_helloFunction.py
@@ -0,0 +1,27 @@
+# Day 2: Dec 4, 2023
+# YouTube Link
+# https://youtu.be/nLRL_NcnK-4?si=HCmer8vpjqvXFKbw
+# From 01:35:00
+
+# create hello the function
+def hello():
+        print("hello")
+
+name = input("What's your name? ")
+hello()
+print(name)
+
+# pass variable into function
+def hello2(name):
+        print("hello,", name)
+
+name = input("What's your name? ")
+hello2(name)
+
+# pass variable into function with the defualt value
+def hello3(name = "world"):
+        print("hello,", name)
+
+hello3()
+name = input("What's your name? ")
+hello2(name)
