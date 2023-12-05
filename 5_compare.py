26de0650f762803dd0ee11f5408fae198b508ca9
akannika <a.kannika80@gmail.com>
2023-12-04 18:18:16 -0800

    conditionals

---
+++ 5_compare.py
@@ -0,0 +1,15 @@
+# Day 2: Dec 4, 2023
+# YouTube Link
+# https://youtu.be/nLRL_NcnK-4?si=HCmer8vpjqvXFKbw
+# From 01:50:41
+
+# compare values: conditionals
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y:
+        print("x is less than y")
+elif x==y:
+        print("x equals y")
+else:
+        print("x is greater than y")
