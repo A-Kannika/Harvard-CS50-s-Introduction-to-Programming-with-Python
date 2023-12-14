b19913381c99850c31d562b36058e1c58a01e48b
akannika <a.kannika80@gmail.com>
2023-12-13 21:15:48 -0800

    List, Dictionary, and List of Dict

---
+++ 10_hogwart.py
@@ -0,0 +1,63 @@
+# Day 3: Dec 13, 2023
+# YouTube Link
+# https://youtu.be/nLRL_NcnK-4?si=HCmer8vpjqvXFKbw
+# From 03:23:00
+
+def main():
+        # List
+        students = ["Hermione", "Harry", "Ron", "Draco"]
+
+        # Dictionary
+        students_houses = {
+                "Hermione" : "Gryffindor", 
+                "Harry": "Gryffindor", 
+                "Ron": "Gryffindor", 
+                "Draco": "Slytherin"
+        }
+
+        # List of Dictionary
+        students_house_patronus = [
+                {"name" : "Hermione", "house" : "Gryffindor", "patronus": "Otter"},
+                {"name" : "Harry", "house" : "Gryffindor", "patronus": "Stag"},
+                {"name" : "Ron", "house" : "Gryffindor", "patronus": "Jack Russell Terrir"},
+                {"name" : "Draco", "house" : "Slytherin", "patronus" : None}
+        ]
+        
+        print_students(students)
+        print()
+        print_students_with_number(students)
+        print()
+        print_student_with_house(students_houses)
+        print()
+        print_list_of_dict(students_house_patronus)
+
+def print_students(students):
+        for student in students:
+                print(student)
+
+def print_students_with_number(students):
+        for i in range(len(students)):
+                print(i + 1, students[i])
+
+def print_student_with_house(students_houses):
+        # use the key as an index
+        for student in students_houses:
+                print(student, students_houses[student], sep=", ")
+
+def print_list_of_dict(students_house_patronus):
+        # print out all students names
+        for student in students_house_patronus:
+                print(student["name"])
+        
+        print()
+                
+        # print out all students names with the houses
+        for student in students_house_patronus:
+                print(student["name"], student["house"], sep=", ")
+        
+        print()
+                
+        # print out all students names with the houses and the patronus
+        for student in students_house_patronus:
+                print(student["name"], student["house"], student["patronus"], sep=", ")
+main()
--- 9_cat.py
+++ 9_cat.py
@@ -1,15 +1,26 @@
 # Day 3: Dec 13, 2023
 # YouTube Link
 # https://youtu.be/nLRL_NcnK-4?si=HCmer8vpjqvXFKbw
-# From 02:47:30 
+# From 02:47:30 - 03:23:00
 
 # Loops
 
 def main() :
-        x = int(input("How many times do you want to print \"meow\"? "))
+        x = valid_input()
         meow(x)
         print()
         meow2(x)
+        print()
+        meow3(x)
+
+def valid_input() :
+        while True:
+                x = int(input("How many times do you want to print \"meow\"? "))
+                if x > 0:
+                        break
+                else:
+                        print ("Please provide the positive integer.")
+        return x
 
 def meow(x) :
         while (x > 0):
@@ -17,7 +28,11 @@ def meow(x) :
                 x -= 1
 
 def meow2(x) :
-        for i in range(x):
+        # We use _ because we don't need to use this variable
+        for _ in range(x):
                 print("meow")
-                
+
+def meow3(x) :
+        print("meow\n" * x, end="")
+
 main()
