6de14527ad22fd976a1f0950be47ab9bdf95ee44
akannika <a.kannika80@gmail.com>
2023-12-22 19:51:19 -0800

    @classmethod

---
+++ 36_type.py
@@ -0,0 +1,14 @@
+# Day 8: Dec 20, 2023
+# YouTube Link
+# https://youtu.be/nLRL_NcnK-4?si=HCmer8vpjqvXFKbw
+# From 12:24:30 - 12:30:00
+
+def main():
+        print(type(30))
+        print(type([]))
+        print(type({}))
+        print(type(list()))
+
+
+if __name__ == "__main__":
+        main()
---
+++ 37_hat.py
@@ -0,0 +1,42 @@
+# Day 8: Dec 20, 2023
+# YouTube Link
+# https://youtu.be/nLRL_NcnK-4?si=HCmer8vpjqvXFKbw
+# From 12:30:00
+
+import random
+
+# see how to use it in function hat()
+class Hat:
+        # placeholder
+        # ...
+
+        def __init__(self):
+                self.house = ["Gryffindor", "Huflepuff", "Ravenclaw", "Slyntherin"]
+
+        def sort(self, name):
+                print(name, "is in", random.choice(self.house))
+
+# use the @classmethod
+# see how to use it in function hat2()
+class Hat2:
+        house = ["Gryffindor", "Huflepuff", "Ravenclaw", "Slyntherin"]
+
+        @classmethod
+        def sort(cls, name):
+                print(name, "is in", random.choice(cls.house))
+
+def main():
+        hat()
+        hat2()
+
+# class Hat
+def hat():
+        hat = Hat()
+        hat.sort("Harry")
+
+# use class Hat2
+def hat2():
+        Hat2.sort("Harry")
+
+if __name__ == "__main__":
+        main()
