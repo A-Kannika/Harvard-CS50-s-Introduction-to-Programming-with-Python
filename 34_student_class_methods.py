c8da6277562c2470c174617a35fcba15f3b880e1
akannika <a.kannika80@gmail.com>
2023-12-22 17:08:11 -0800

    Create our own method

--- 34_student_class_methods.py
+++ 34_student_class_methods.py
@@ -9,31 +9,47 @@ class Student:
         # we will be able to customize this class object more
         # it's an instance method to initialize 
 
-        def __init__(self, name, house):
+        def __init__(self, name, house, patronus):
                 # use raise to catch an exception
                 if not name:
                         raise ValueError("Missing Name")
                 if house in ["Gryffindor", "Huflepuff", "Ravenclaw", "Slyntherin"]:
                         raise ValueError("Invalid house")
                 self.name = name
-                self.house = house 
+                self.house = house
+                self.patronus = patronus
 
+        def __str__(self):
+                return f"{self.name} from {self.house}"
+        
+        # create our own funtion
+        def charm(self):
+                match self.patronus:
+                        case "Stag":
+                                return "🐴"
+                        case "Otter":
+                                return "🦦"
+                        case "Jack Russell terier":
+                                return "🐶"
+                        case _:
+                                return "🐟"
+                        
 def main():
         student = get_student()
-        print(f"{student.name} from {student.house} from get_student")
+        print("Expecto Patronum!")
+        # print(f"{student.name} from {student.house}")
+        print (student.charm())
 
 def get_student():
         name = input("Name: ")
         house = input("House: ")
+        patronous = input("Patronous: ")
 
         # passing an arguments to the Student class
         # treating the Student class as a function
         # we will have more control on this 
         # retrun the Student object
-        return Student(name, house)
-
-
-
+        return Student(name, house, patronous)
 
 if __name__ == "__main__":
         main()
